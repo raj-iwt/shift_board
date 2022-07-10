a8e4c836470395fb0853a17f9bbe046a54d18342
Rajkumar Palaniyappan <rpalaniyappan@adventintermodal.com>
2022-07-09 22:49:45 -0700

    refactoring code

--- src/services/rule.service.js
+++ src/services/rule.service.js
@@ -1,3 +1,5 @@
+const defaultShiftCode = "-";
+
 function RuleService() {
   var _self = this;
 
@@ -7,50 +9,68 @@ function RuleService() {
     updateInfo,
     employeeSchedule
   ) {
-    console.log(employeeSchedule.shifts);
-    console.log(employeeShift);
-    if (updateInfo.shiftCode !== "-") {
-      let searchMore = true;
-      const previous_key = moment(employeeShift.shiftDate);
-      const next_key = moment(employeeShift.shiftDate);
-      console.log('The previous_key :', previous_key.format('L'));
-      console.log('The next_key :', next_key.format('L'));
-
-      let previous_shift_ends = false;
-      let next_shift_ends = false;
-      let occurrence = 0;
-
-      while (searchMore) {
-        const previous = previous_key.subtract(1, 'days').format('L');
-        const next = next_key.add(1, 'days').format('L');
-        const previousShift = employeeSchedule.shifts.find(s => previous === s.shiftDate.format('L'));
-        const nextShift = employeeSchedule.shifts.find(s => next === s.shiftDate.format('L'));
-        console.log('the previous shift :', previous);
-        console.log('the next shift :', next);
-
-        if (previous_shift_ends || previousShift.shiftCode === '-'){
-          previous_shift_ends = true;
-        }
-        else {
-          occurrence++;
-        }
-
-        if (next_shift_ends || nextShift.shiftCode === '-'){
-          next_shift_ends = true;
-        }
-        else {
-          occurrence++;
-        }
-
-        if (occurrence >= 5 || (previous_shift_ends && next_shift_ends)){
-          searchMore = false;
-        }
+    if (updateInfo.shiftCode !== defaultShiftCode) {
+      if (ruleId === Rules.notMoreThanFiveWorkDaysInARow) {
+        return _rule101(employeeShift, employeeSchedule);
       }
-
-      console.log('The total :', occurrence);
-      return occurrence < 5;
     }
-
     return true;
   };
+
+  function _rule101(employeeShift, employeeSchedule) {
+    let searchMore = true;
+    const fwd_key = moment(employeeShift.shiftDate);
+    const bw_key = moment(employeeShift.shiftDate);
+    console.log("The forward key :", fwd_key.format("L"));
+    console.log("The backword key :", bw_key.format("L"));
+
+    let fwd_check_ended = false;
+    let bw_check_ends = false;
+    let shift_count = 0;
+
+    while (searchMore) {
+      const fwd_date = fwd_key.subtract(1, "days").format("L");
+      const bw_date = bw_key.add(1, "days").format("L");
+
+      const fwd_shift = findShift(fwd_date, employeeSchedule);
+      const bw_shift = findShift(bw_date, employeeSchedule);
+
+      if (!fwd_check_ended) {
+        fwd_check_ended = check_off_work(fwd_shift);
+        shift_count += count_work_day(fwd_shift);
+      }
+
+      if (!bw_check_ends) {
+        bw_check_ends = check_off_work(bw_shift);
+        shift_count += count_work_day(bw_shift);
+      }
+
+      if (fwd_check_ended && bw_check_ends) {
+        searchMore = false;
+      }
+
+      if (shift_count >= 5) {
+        searchMore = false;
+      }
+    }
+
+    console.log("The total :", shift_count);
+    return shift_count < 5;
+  }
+
+  function count_work_day(shift) {
+      return shift.shiftCode === defaultShiftCode ? 0 : 1;
+  }
+
+  function check_off_work(shift) {
+    return shift.shiftCode === defaultShiftCode;
+  }
+
+  function findShift(key, employeeSchedule) {    
+    const shift = employeeSchedule.shifts.find(
+      (s) => key === s.shiftDate.format("L")
+    );
+    console.log("Found the shift:", shift);
+    return shift;
+  }
 }
